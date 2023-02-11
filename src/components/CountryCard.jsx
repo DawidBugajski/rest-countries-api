3a57f3a1770acfcb09daf550c537ab42767eb4e8
Dawid <dawidbugajski1996@gmail.com>
2023-02-11 10:11:37 +0100

    start implementing header

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -3,14 +3,26 @@ import React from 'react';
 const CountryCard = ({ countryData }) => {
   return countryData.map((country) => (
     <div
-      className='w-8/12 mb-10 bg-orange-300 rounded-lg h-72'
+      className='w-9/12 mb-10 border-2 border-gray-100 rounded-md h-96'
       key={country.countryName}
     >
       <img
-        className='w-full rounded-tl-lg h-1/2'
+        className='w-full rounded-t-md h-1/2  border-b-[1px] border-gray-200'
         alt={country.countryName}
         src={country.flagUrl}
       />
+      <div className='pt-6 pl-6'>
+        <h2 className='pb-4 text-xl font-extrabold'>{country.countryName}</h2>
+        <p className='font-semibold'>
+          Population: <span className='font-light'>{country.population}</span>
+        </p>
+        <p className='font-semibold'>
+          Region: <span className='font-light'>{country.region}</span>
+        </p>
+        <p className='font-semibold'>
+          Capital: <span className='font-light'>{country.capital}</span>
+        </p>
+      </div>
     </div>
   ));
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='flex items-center justify-between h-24 pl-4 border-b-2 shadow-lg border-LM_VeryLightGray'>
+      <h1 className='font-extrabold'>Where in the world?</h1>
+      <div className='flex justify-center pr-4'>
+        <p>ikonka</p>
+        <p>Dark Mode</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
