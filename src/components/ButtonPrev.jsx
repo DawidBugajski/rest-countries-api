d4b517aa985e7dbce202c5a5a94f47924f04c889
Dawid <dawidbugajski1996@gmail.com>
2023-02-19 14:35:56 +0100

    implement 404page for light mode

--- src/components/ButtonPrev.jsx
+++ src/components/ButtonPrev.jsx
@@ -7,7 +7,7 @@ const ButtonPrev = () => {
   const { setSearchTerm } = useContext(CountryContext);
   const handleResetSearchTerm = () => setSearchTerm('');
   return (
-    <div className=' mt-8 shadow-lg border-[1px] w-24 py-1 rounded-sm 2xl:w-32 2xl:text-xl 2xl:mb-20'>
+    <div className=' mt-8 shadow-lg border-[1px] w-24 py-1 rounded-sm 2xl:w-32 2xl:text-xl 2xl:mb-20 hover:text-blue-900'>
       <Link
         onClick={handleResetSearchTerm}
         className='flex items-center justify-center '
--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Oops!</h1>
+    <div className='w-11/12 mx-auto text-lg text-center 2xl:text-xl'>
+      <img
+        className='w-10/12 mx-auto mt-12 lg:w-2/3 xl:w-1/2 2xl:w-2/5'
+        src='/images/ErrorPage.svg'
+        alt='404page'
+      />
+      <h1 className='my-4 text-3xl 2xl:text-5xl'>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
+      <Link to='/'>
+        <p className='font-semibold text-blue-900 hover:text-purple-600'>
+          Back to the main page
+        </p>
+      </Link>
     </div>
   );
 };
