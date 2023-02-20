e62e9498079b4bf6e7866f4ec0a62b119b487098
Dawid <dawidbugajski1996@gmail.com>
2023-02-20 17:53:42 +0100

    changes

--- src/components/NoResultsFound.jsx
+++ src/components/NoResultsFound.jsx
@@ -5,7 +5,7 @@ const NoResultsFound = () => {
     <div className='text-xl text-center md:text-2xl dark:text-white'>
       <img
         className='w-2/3 mx-auto my-12 sm:w-1/2 md:w-2/3 lg:w-2/3 2xl:w-1/2'
-        src='images/noresoult.svg'
+        src={`${process.env.PUBLIC_URL}/images/noresoult.svg`}
         alt='person watchs blackhole'
       />
       <p className=''>The data given does not match any country.</p>
--- src/routes/Error404.jsx
+++ src/routes/Error404.jsx
@@ -6,7 +6,7 @@ const Error404 = () => {
     <div className='w-11/12 mx-auto text-lg text-center 2xl:text-xl dark:text-white'>
       <img
         className='w-10/12 mx-auto mt-12 lg:w-2/3 xl:w-1/2 2xl:w-2/5'
-        src='images/404.svg'
+        src={`${process.env.PUBLIC_URL}/images/404.svg`}
         alt='404page'
       />
       <h1 className='my-4 text-3xl 2xl:text-5xl'>Oops!</h1>
