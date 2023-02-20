1352e68bcd41589991d28f3eb7c29fca56af0e89
Dawid <dawidbugajski1996@gmail.com>
2023-02-20 21:51:51 +0100

    fix bug on xs with borderCountries

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='h-screen dark:bg-DM_VeryDarkBlue'>
+    <div className='min-h-full dark:bg-DM_VeryDarkBlue'>
       <CountryProvider>
         <Header />
         <Routes>
