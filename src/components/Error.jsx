da197650e0b33855889537ebac26adbe73dded53
Dawid <dawidbugajski1996@gmail.com>
2023-02-20 16:22:31 +0100

    changed path images

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,7 @@ const Error = ({ errorMessage }) => {
     <div className='text-xl text-center lg:text-2xl dark:text-white'>
       <img
         className='mx-auto my-12 md:w-2/3 lg:w-2/3 xl:w-1/2 2xl:w-2/5'
-        src='/images/error.svg'
+        src='images/error.svg'
         alt='computer'
       />
       <p className=''>An error has occurred</p>
